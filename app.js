985dd95718ad48ba717f87405a3d7eddf7de6c42
islamhamdaoui <islamhamdaoui2000@gmail.com>
2024-06-05 23:22:13 +0100

    Added new voices

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-let speech =  new SpeechSynthesisUtterance();
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
 
-function convert(){
-   let textarea= document.querySelector("textarea").value
-      speech.text = textarea
-      window.speechSynthesis.speak(speech);
+let select = document.querySelector('select')
 
+window.speechSynthesis.onvoiceschanged = ()=>{
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0]
+
+    voices.forEach((voice ,i)=> (select.options[i] = new Option(voice.name, i)))
+
+    
+}
+
+select.addEventListener("change",()=> {
+    speech.voice = voices[select.value];
+})
+
+function convert() {
+  let textarea = document.querySelector("textarea").value;
+  speech.text = textarea;
+  window.speechSynthesis.speak(speech);
 }
